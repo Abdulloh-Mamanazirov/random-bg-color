d6934f8225cbf187fe2eea6ff390116d5ecd8a54
Abdulloh <abdullajonmamnazirov@gmail.com>
2022-11-29 22:15:32 +0500

    added button

--- Bulb on--off/app.js
+++ Bulb on--off/app.js
@@ -1,11 +1,12 @@
 let image = document.querySelector(".bulb");
 let body = document.getElementsByTagName("body")[0];
-
+/*
 setInterval(() => {
   let src = image.getAttribute("src");
 //   let bor = body.classList.contains("shadow");
   if (src === "images/light-on.png") {
     // body.classList.add("shadow");
+    body.style.backgroundColor = "#1c1c1c";
     image.setAttribute("src", "images/light-off.png");
   }else {
     // body.classList.remove("shadow");
@@ -13,3 +14,27 @@ setInterval(() => {
     image.setAttribute("src", "images/light-on.png");
   }
 }, 2000);
+*/
+let src = image.getAttribute("src");
+document.getElementById('on').onclick = function(){
+  body.style.backgroundColor = "yellow";
+  image.setAttribute("src", "images/light-on.png");
+}
+document.getElementById('off').onclick = function(){
+  body.style.backgroundColor = "#1c1c1c";
+  image.setAttribute("src", "images/light-off.png");
+}
+/*
+let on = document.getElementsByTagName("button")[0];
+
+on.addEventListener("click", function onClick() {
+  on.style.backgroundColor = "blue";
+  on.style.color = "white";
+});
+let off = document.getElementsByTagName("button")[1];
+
+off.addEventListener("click", function onClick() {
+  off.style.backgroundColor = "blue";
+  off.style.color = "white";
+});
+*/
